66de82987b71f1d12d6ed68251d8d1bb87929c08
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-07-01 15:55:10 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+
+const userRouter = express.Router();
+
+
+const userController = require("../controllers/userController");
+
+userRouter.post("/", userController.register);
+userRouter.put("/forgot", userController.forgot);
+userRouter.get("/verify/:key", userController.verify);
+userRouter.put("/reset", userController.reset);
+
+
+module.exports = userRouter;
